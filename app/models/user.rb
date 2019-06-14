b93ae504f525e74c5e48a55f27422ad358ae3505
marycriv <maryecrivelli@gmail.com>
2019-06-14 15:42:10 -0400

    Created seeds file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :pressings, through: :coins
   has_many :coins
+  validates :username, uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroying now"
+User.destroy_all
+Pressing.destroy_all
+Coin.destroy_all
+puts "Destroying complete"
+
+puts "Creating users"
+u1 = User.create(username: "mary", password: "mary1", display_name: "mary criv")
+u2 = User.create(username: "chris", password: "chris1", display_name: "chris yuan")
+u3 = User.create(username: "rishi", password: "rishi1", display_name: "squishy")
+u4 = User.create(username: "kevin", password: "kevin1", display_name: "kebin")
+u5 = User.create(username: "jaimie", password: "jaimie1", display_name: "jamjam")
+puts "Users are created"
+
+
+
+puts "Creatin pressins"
+p1 = Pressing.create(name: "sexy goldblum", image_url: "goldblum.png")
+p2 = Pressing.create(name: "somber goldblum", image_url: "goldblum.png")
+p3 = Pressing.create(name: "boring goldblum", image_url: "goldblum.png")
+puts "Pressins be done"
+
+puts "Creatin some coins"
+c1 = Coin.create(user_id: u1.id, pressing_id: p1.id)
+c2 = Coin.create(user_id: u2.id, pressing_id: p2.id)
+c3 = Coin.create(user_id: u3.id, pressing_id: p3.id)
+c4 = Coin.create(user_id: u4.id, pressing_id: p2.id)
+c5 = Coin.create(user_id: u5.id, pressing_id: p1.id)
+puts "Coins be created"
