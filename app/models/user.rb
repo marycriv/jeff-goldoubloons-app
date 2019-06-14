c1ddf1be306a4c6643bdc081b7115ded76356d3f
marycriv <maryecrivelli@gmail.com>
2019-06-14 16:35:22 -0400

    Edited user views

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :pressings, through: :coins
   has_many :coins
   validates :username, uniqueness: true
+
+  def coin_name
+    self.coins.select{|c| c.pressing }
+  end
 end
