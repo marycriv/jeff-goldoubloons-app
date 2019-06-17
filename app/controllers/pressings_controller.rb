6de4c618b1e57fd724fc9cb0a5d1561a19c1ae42
marycriv <maryecrivelli@gmail.com>
2019-06-17 09:32:01 -0400

    Updated pressings controller

--- app/controllers/pressings_controller.rb
+++ app/controllers/pressings_controller.rb
@@ -1,13 +1,34 @@
 class PressingsController < ApplicationController
   def index
+    @pressings = Pressing.all
   end
 
   def show
+    @pressing = Pressing.find(params[:id])
   end
 
   def new
+    @pressing = Pressing.new
+  end
+
+  def create
+    @pressing = Pressing.create(pressing_params)
+    redirect_to @pressing
   end
 
   def edit
+    @pressing = Pressing.find(params[:id])
   end
+
+  def update
+    @pressing = Pressing.update(pressing_params)
+    redirect_to @pressing
+  end
+
+  private
+
+  def pressing_params
+    params.require(:pressing).permit(:name, :img_url)
+  end
+
 end
