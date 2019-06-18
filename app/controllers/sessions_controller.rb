498d5323185d27e92120dc0ac6c387c13397470a
marycriv <maryecrivelli@gmail.com>
2019-06-18 11:48:45 -0400

    Login successful

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,13 @@ class SessionsController < ApplicationController
 
   def create
     #authenticate the user
-    user = User.find_by(params[:username])
-    if user && user.authenticate(params[:password])
+    user = User.find_by(username: params[:login_creds][:username])
+    if user && user.authenticate(params[:login_creds][:password])
       log_in(user)
-      redirect_to user
+      redirect_to '/home'
     else
       flash.now[:danger] = 'Invalid username/password combination'
+      redirect_to '/home'
     end
   end
 
