da94a8af97915ed833800065b59ca410f961c410
marycriv <maryecrivelli@gmail.com>
2019-06-19 20:52:37 -0400

    Add icon link in user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password, :display_name, :wallet)
+    params.require(:user).permit(:username, :password, :display_name, :wallet, :icon)
   end
 
 end
